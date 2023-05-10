df946ae6fb6bf3c80e1ffea70bc08415b655072f
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-05-10 14:05:10 -0300

    feat: create price comparison in backend

--- backend/src/services/data/buscapeData.js
+++ backend/src/services/data/buscapeData.js
@@ -8,6 +8,7 @@ const buscapeProducts = async (category, { id: searchId, description: search }) 
   const { data } = await axios.get(`${siteUrl}/${buscapeCategory}`);
   const $ = cheerio.load(data);
   const allProducts = [];
+  const productsList = [];
   const { id } = await Category.findOne({ where: { name: category } });
 
   $('.col-lg-9 div').each(async (e, i) => {
@@ -32,21 +33,34 @@ const buscapeProducts = async (category, { id: searchId, description: search }) 
     (product) => product.price && typeof product.title === 'string' && product.title.length < 100,
   );
 
-  const getValidImage = async (product) => {
+  const getProductData = async (product) => {
     const fileData = await axios.get(product.linkUrl);
     const $$ = cheerio.load(fileData.data);
     const imageSrc = $$('.ProductPageBody_ContentBody__De_1M')
       .find('div:nth-child(2) img')
       .attr('src');
+
+    $$('.Offers_OfferListWrapper__f1By_').each(async (_e, i) => {
+      const bestOffer = {
+        info: $$(i).find('li .OfferCard_OfferCardWrapper__3SbhD .OfferCard_WithBestOffer__zXEaM  a .OfferPrice_PriceContent__MW3Ty').text(),
+        productImg: $$(i).find('li .OfferCard_OfferCardWrapper__3SbhD .OfferCard_WithBestOffer__zXEaM  a img').attr('src'),
+        company: $$(i).find('li .OfferCard_OfferCardWrapper__3SbhD .OfferCard_WithBestOffer__zXEaM .OfferCard_OfferCardFooter__9dsDN a h3').text(),
+        companyImg: $$(i).find('li .OfferCard_OfferCardWrapper__3SbhD .OfferCard_WithBestOffer__zXEaM .OfferCard_OfferCardFooter__9dsDN a div img').attr('src'),
+        link: $$(i).find('li .OfferCard_OfferCardWrapper__3SbhD .OfferCard_WithBestOffer__zXEaM  a').attr('href') || '',
+      };
+      productsList.push(bestOffer);
+    });
+
     return {
       ...product,
       imageUrl: imageSrc,
+      products: productsList,
     };
   };
 
-  const images = await Promise.all(filteredProducts.map(getValidImage));
+  const productData = await Promise.all(filteredProducts.map(getProductData));
 
-  return images;
+  return productData;
 };
 
 module.exports = buscapeProducts;
