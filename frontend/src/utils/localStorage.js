e36d32a3c1d247e2db92af191592250fb7c5aef5
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-05-25 19:02:32 -0300

    fix: localStorage exportations

--- frontend/src/utils/localStorage.js
+++ frontend/src/utils/localStorage.js
@@ -1,14 +1,14 @@
-const getFav = () => {
+export const getFav = () => {
   const items = JSON.parse(localStorage.getItem('favorite')) || []
   return items
 }
 
-const favAdd = (product) => {
+export const favAdd = (product) => {
   const items = getFav()
   localStorage.setItem('favorite', JSON.stringify([...items, product]))
 }
 
-const favRem = (item) => {
+export const favRem = (item) => {
   const items = getFav()
   const itemToRemove = items.findIndex((i) => i.title === item.title)
   if (itemToRemove !== -1) {
@@ -17,17 +17,17 @@ const favRem = (item) => {
   }
 }
 
-const getCart = () => {
+export const getCart = () => {
   const items = JSON.parse(localStorage.getItem('cart')) || []
   return items
 }
 
-const cartAdd = (product) => {
+export const cartAdd = (product) => {
   const items = getCart()
   localStorage.setItem('cart', JSON.stringify([...items, product]))
 }
 
-const cartRem = (item) => {
+export const cartRem = (item) => {
   const items = getCart()
   const itemToRemove = items.findIndex((i) => i.title === item.title)
   if (itemToRemove !== -1) {
@@ -35,5 +35,3 @@ const cartRem = (item) => {
     localStorage.setItem('cart', JSON.stringify(items))
   }
 }
-
-module.exports = { getFav, favAdd, favRem, getCart, cartAdd, cartRem }
