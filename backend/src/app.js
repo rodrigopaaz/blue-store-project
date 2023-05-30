6d726b379d917a6038c7d1f86f1317f1f86836ca
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-05-30 19:06:19 -0300

    fix: cors error, remove cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,7 @@
 const express = require('express');
-const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-
-app.use(cors());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://blue-store-project-as3i.vercel.app');
