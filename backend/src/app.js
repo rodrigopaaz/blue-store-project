c9a12a9c40158ab9a49a37821f04d70cf8b0c49e
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-08-01 14:48:32 -0300

    fix: cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,8 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://blue-store-project-as3i.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://blue-store-project-as3i-gla7jnwr1-rodrigopaaz.vercel.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
